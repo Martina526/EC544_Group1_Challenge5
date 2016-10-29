fe1972d178fca8816d69e94df324be04a9cf0970
Martina526 <ymy@bu.edu>
2016-10-28 17:04:02 -0700

    Update handelerOnParticle.ino

--- TRY/handelerOnParticle.ino
+++ TRY/handelerOnParticle.ino
@@ -1,79 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="js/jquery-2.1.4.js"></script>
-<script src="js/main.js"></script> <!-- Resource jQuery -->   
-<link href = "static/source.css" rel = "stylesheet">   
-<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
-<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
-<script src="js/modernizr.js"></script> <!-- Modernizr -->    
-    <title>Control Page</title>
-</head>
-<body>  
-<div id="wrapper" style = "">
-    <div id="info">
-        <div align = "center"><h1>Control Page</h1></div>
-        <div id = "container" style = " width : 750px; height : 400px;  margin-left: 200px; margin-top:50px" >
-            <button onclick = 'start_the_car'> START </button>
-            <script type="text/javascript">
-	           function start_the_car(){
-                    var url = 'https://api.spark.io/v1/devices/xxx/events/start';
-                    var es = new EventSource(url);
-               }
-            </script>
-            <button onclick = 'stop_the_car'> START </button>
-            <script type="text/javascript">
-	           function stop_the_car(){
-                    var url = 'https://api.spark.io/v1/devices/xxx/events/dontHit';
-                    var es = new EventSource(url);
-               }
-            </script>
-            <!--
-                Here comes a controling bar/list to control the speed with a button to click to control the speed of the car
-            -->
-            <button onclick = 'change_speed'> START </button>
-            <!--var speed = some value in the control bar/list, such as: 
-                ################1
-                var speed = document.getElementById('selector');
-                ################1
-                Also if we choose to use selector, we need to create a selector and also append a js after it
-                ################2
-                <select id = "selector1" ></select>
-                for(var index = 0; index <= gap/truegap; index++){
-                    t.setMilliseconds(t.getMilliseconds()+ truegap);
-                    $("#selector1").append("<option value ="+index +">" + t + "</option>");
-                    $("#selector2").append("<option value ="+index +">" + t + "</option>");
-                    //alert(t);
-                }
-                ################2
-            -->
-            <script type="text/javascript">
-	           function stop_the_car(speed){
-                    var url = 'https://api.spark.io/v1/devices/xxx/events/controlSpeed/:'+speed;
-                    var es = new EventSource(url);
-               }
-            </script>
+
+Servo wheels; 
+Servo esc; 
+double speed;
+double wheelOffset;
+double output;
+
+void setup(){
+  wheels.attach(D1); 
+  esc.attach(D0); 
+}
+
+void controlWheel(const char *event, const char *data)
+{
+    var wheelOffset = output*5 * sin(90);
+    //wheelOffset = double(data);// String.toDouble()???
+    wheels.write(wheelOffset);
+    //Try check by following
+    Particle.publish(String "currWheelOffset", String String(data), double wheelOffset);
+}
+
+void controlSpeed(const char *event, const char *data)
+{
+    speed = double(data);
+    esc.write(speed); 
+    //Try check by following
+    Particle.publish(String "currSpeed", String String(data), double speed);
+}
+
+// Could use for both "stop" buttom & don't hith the wall feature.
+// Automatically not gonna work if detected the car already stopped.
+void immediatelyStop(const char *event, const char *data)
+{
+    if(speed != 0){
+        speed = 0;
+        esc.write(0); 
+    }
+}
+
+//Work for the "start" buttom. 
+//Automatically not gonna work if detected the car already in speed.
+void defaultStart(const char *event, const char *data)
+{
+    if(speed == 0){
+        speed = 20;
+        esc.write(speed); 
+    }
     
-        </div>
-    </div>
-<div id="info-content"></div>
-</div>
-    
-    <!--<input type="checkbox" id="menu" name="menu" class="menu-checkbox">-->
-<div class="menu">
-    <label class="menu-toggle" for="menu"><span>Toggle</span></label>
-    <ul>
-      <li>
-        <a href="http://ec544group1.hopto.org:3002">Home</a>
-      </li>
-      <li>
-        <a href="http://ec544group1.hopto.org:3002/realtime">Control Page</a>
-      </li>
-    </ul>
-</div>
-</body>
-</html>
+}
+
+void setup()
+{
+    Particle.subscribe("controlWheel", controlWheel);
+    Particle.subscribe("controlSpeed", controlSpeed);
+    Particle.subscribe("dontHit", immediatelyStop);
+    Particle.subscribe("start", defaultStart)
+    Serial.begin(9600);
+}
