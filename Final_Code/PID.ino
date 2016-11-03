bc3bd4094365a991930f25c21e56b7d8e44cc18e
xuanmo0213 <xuanmo0213@gmail.com>
2016-11-03 11:07:58 -0400

    Correct and delete comments

--- Final_Code/PID.ino
+++ Final_Code/PID.ino
@@ -3,13 +3,10 @@
 Servo wheels; // servo for turning the wheels
 Servo esc; // not actually a servo, but controlled like one!
 double pulse_width_front, pulse_width_back;
-//int analogvalue;
-//int sonar = A0;
 int lidar_front = D3;
 int lidar_back = D4;
 int startupDelay = 1000;
 int control;
-//int led = D7;
 double Setpoint, Input, Output, Gap, input_raw, Offset, c_output, d_diff;
 double aggKp=1, aggKi=0.0005, aggKd=0.15; 
 double consKp=1, consKi=0.00005, consKd=3=0.5;
@@ -18,35 +15,29 @@ double flag = 1, c_flag = 1;
 int angle, c_angle;
 
 PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, PID::DIRECT);
-//PID correctPID(&d_diff, &c_output, &Offset, consKp, consKi, consKd, PID::DIRECT);
  
 void setup(){
-  
-  //distance = analogRead(A0); 
-  //pinMode(D6,INPUT);
+
   Particle.function("start",start);
   Particle.function("stop", stop);
+ 
   pulse_width_front = pulseIn(D3, HIGH);
   pulse_width_back = pulseIn(D4, HIGH);
   delay(50);
   Input = abs(pulse_width_front-pulse_width_back);
   Offset = (pulse_width_front + pulse_width_back)/20;
   //Particle.publish("Connected");
+ 
   Setpoint = 4;
   Gap = abs(Setpoint-Input);
   myPID.SetMode(PID::AUTOMATIC);
-  //wheels.detach(D0);
   wheels.attach(D0);
-  //esc.attach(A4);// initialize wheel servo to Digital IO Pin #8
-  esc.attach(D1); // initialize ESC to Digital IO Pin #9
+  esc.attach(D1); 
   wheels.write(95);
-  pinMode(lidar_front, INPUT); // Set pin 3 as monitor pin
+  pinMode(lidar_front, INPUT); 
   pinMode(lidar_back, INPUT);
-  //pinMode(sonar, INPUT);
   calibrateESC();
-  
-  
-  //pinMode(led, OUTPUT);
+
 }
  void calibrateESC(){
     esc.write(180); // full backwards
@@ -60,9 +51,8 @@ void setup(){
  
   void oscillate(){
     distance = analogRead(A0);
-    //int control = digitalRead(D6);
    // Particle.publish("Stop",String(distance));
-    if (distance >= 300 && control == 1){// && control != 0){
+    if (distance >= 300 && control == 1){
         esc.write(50);
         //delay(10);
         pulse_width_front = pulseIn(D3, HIGH);
@@ -75,15 +65,12 @@ void setup(){
         if ( pulse_width_front !=0 && pulse_width_back != 0 && pulse_width_front <= 5000 && pulse_width_front <= 5000){
             pulse_width_front = pulse_width_front/10; // 10usec = 1 cm of distance for LIDAR-Lite
             //Particle.publish("lidar1",String(pulse_width_front));
-  // Print the distance
             pulse_width_back = pulse_width_back/10;
             //Particle.publish("lidar2",String(pulse_width_front) + "+" + String(pulse_width_back));
   
             input_raw = pulse_width_front - pulse_width_back;
-  //d_diff = (pulse_width_front + pulse_width_back)/2 - Offset;
             Input =0 - abs(input_raw);
-  //Particle.publish("Input",String(Input));
-  //Particle.publish("Input", String(Input));
+
             Gap = abs( Input - Setpoint ); 
             if (Gap > 60){
                 Output=0;
@@ -124,20 +111,15 @@ void setup(){
     }
    // Particle.publish("control",String(control));
     //delay(1000);
-   //digitalWrite(led, LOW);
    
   }                                                                                                                                                                                                              
 
 void loop(){
     oscillate();
-  //esc.write(60);
-  //digitalWrite(led, HIGH);
-  
+ 
    
 }
 int stop(String str) {
-   // digitalWrite(control, LOW);
-   // digitalWrite(D7, LOW);
     //Particle.publish("stop","stop");
     //int b = Particle.publish("")
     control = 0;
@@ -145,14 +127,11 @@ int stop(String str) {
 }
 
 int start(String str) {
-   // digitalWrite(D7, HIGH);
-    //digitalWrite(control, HIGH);
+
    control =1 ;
 
 
    // Particle.publish("start",String(control));
-     //int a = digitalRead(control);
-     //Particle.publish("test", String(a));
 
-    return 0;
+   return 0;
 }
